34def2ff65f2d6e81a2a8658c00b324fb766331c
X-Funny <lts1150869019@outlook.com>
2022-10-08 21:09:50 +0800

    调整

--- app/src/main/java/top/xfunny/servermap/AboutActivity.java
+++ app/src/main/java/top/xfunny/servermap/AboutActivity.java
@@ -1,16 +1,29 @@
 package top.xfunny.servermap;
 
-
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
-import top.xfunny.servermap.ui.activity.WebActivity;
+public class AboutActivity extends AppCompatActivity {
 
-public class AboutActivity extends WebActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
+
+        WebView WebView = (WebView) findViewById(R.id.webview1);
+        WebView.loadUrl("http://112.124.52.20:5500");
+        WebSettings webSettings = WebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
     }
 }
+
+
+
+
+
+
