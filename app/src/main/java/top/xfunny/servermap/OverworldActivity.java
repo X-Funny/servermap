faf7b79a0c5bd630860a1fe3c99ea96bd5d09778
X-Funny <79325059+X-Funny@users.noreply.github.com>
2023-02-02 11:17:56 +0800

    地址修改

--- app/src/main/java/top/xfunny/servermap/OverworldActivity.java
+++ app/src/main/java/top/xfunny/servermap/OverworldActivity.java
@@ -16,7 +16,7 @@ public class OverworldActivity extends WebActivity {
         super.onCreate(savedInstanceState);
 
         getWebSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
-        loadUrl("http://playmc.run:40875/");
+        loadUrl("http://playmc.run:37627/");
 
     }
 
